610b2dcfe852c4eb0a6b411978890b658339faa0
a-san-un <181435805+a-san-un@users.noreply.github.com>
2026-03-09 16:37:18 +0900

    feat: メニューセクションのトップ投稿取得ロジックを改善し、動的表示を実装

--- wp/wp-content/themes/hanacafe-theme/template-parts/home/menu.php
+++ wp/wp-content/themes/hanacafe-theme/template-parts/home/menu.php
@@ -2,8 +2,55 @@
 
 /**
  * Home: Menu Section
+ * [並び順] 左:FOOD / 中:DRINK / 右:DESSERT
+ * [抽出条件] 各カテゴリーで 'is_top' にチェックが入っているものを優先表示
  */
+
+// カテゴリースラッグの定義（管理画面のスラッグと一致させてください）
+$menu_categories = [
+    'food'    => 'food',
+    'drink'   => 'drink',
+    'dessert' => 'dessert'
+];
+
+$top_posts = [];
+
+foreach ($menu_categories as $key => $slug) {
+    $args = [
+        'post_type'      => 'menu',
+        'posts_per_page' => 1,
+        'tax_query'      => [
+            [
+                'taxonomy' => 'menu_category',
+                'field'    => 'slug',
+                'terms'    => $slug,
+            ],
+        ],
+        // 'is_top' が '1' のものを最優先で取得
+        'meta_query'     => [
+            [
+                'key'   => 'is_top',
+                'value' => '1',
+                'compare' => '='
+            ]
+        ]
+    ];
+
+    $query = new WP_Query($args);
+
+    // もし 'is_top' にチェックが入っているものがなければ、最新の1件をバックアップとして取得
+    if (!$query->have_posts()) {
+        unset($args['meta_query']); // meta_queryを解除して再検索
+        $query = new WP_Query($args);
+    }
+
+    if ($query->have_posts()) {
+        $top_posts[] = $query->posts[0];
+    }
+    wp_reset_postdata();
+}
 ?>
+
 <section class="l-section p-menu" id="menu">
     <div class="l-container">
         <div class="p-menu__inner">
@@ -20,37 +67,14 @@
 
             <div class="p-menu__list">
                 <?php
-                $args_top = [
-                    'post_type'      => 'menu',
-                    'posts_per_page' => 3,
-                    'meta_query'     => [['key' => 'is_top', 'value' => '1', 'compare' => '=']]
-                ];
-                $query_top = new WP_Query($args_top);
-                $top_posts = $query_top->posts;
-
-                $count_needed = 3 - count($top_posts);
-                if ($count_needed > 0) {
-                    $exclude_ids = wp_list_pluck($top_posts, 'ID');
-                    $args_fill = [
-                        'post_type'      => 'menu',
-                        'posts_per_page' => $count_needed,
-                        'post__not_in'   => $exclude_ids,
-                        'orderby'        => 'date',
-                        'order'          => 'DESC'
-                    ];
-                    $query_fill = new WP_Query($args_fill);
-                    $top_posts = array_merge($top_posts, $query_fill->posts);
-                }
-
-                if ($top_posts) :
+                if (!empty($top_posts)) :
                     foreach ($top_posts as $post) : setup_postdata($post);
+                        // 写真 -> タイトル -> サブタイトル -> 本文 -> 値段 の順で出力
                         get_template_part('template-parts/loop', 'menu');
                     endforeach;
                     wp_reset_postdata();
-                else :
+                endif;
                 ?>
-                    <p>現在メニューを準備中です。</p>
-                <?php endif; ?>
             </div>
         </div>
     </div>
